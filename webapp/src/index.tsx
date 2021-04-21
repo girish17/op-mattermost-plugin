31c4ffac2b5fa336597f0ef4b12815b6a7be2d4a
girishm <girishm@posteo.net>
2021-04-21 20:55:37 +0530

    1. register /op command 2. create op-mattermost bot

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {Store, Action} from 'redux';
 
 import {GlobalState} from 'mattermost-redux/types/store';
@@ -7,10 +8,27 @@ import manifest from './manifest';
 // eslint-disable-next-line import/no-unresolved
 import {PluginRegistry} from './types/mattermost-webapp';
 
+const Icon = () => <i className='icon fa fa-plug'/>;
+
 export default class Plugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
     public async initialize(registry: PluginRegistry, store: Store<GlobalState, Action<Record<string, unknown>>>) {
         // @see https://developers.mattermost.com/extend/plugins/webapp/reference/
+        registry.registerChannelHeaderButtonAction(
+
+            // icon - JSX element to use as the button's icon
+            <Icon/>,
+
+            // action - a function called when the button is clicked, passed the channel and channel member as arguments
+            // null,
+            () => {
+                alert('Hello World!');
+            },
+
+            // dropdown_text - string or JSX element shown for the dropdown button description
+            'Hello World',
+            'Hello World',
+        );
     }
 }
 
